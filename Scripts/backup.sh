bd6a0ce6630fbaa4dcaaea555720c8fc93ae989d
Nick Ingersoll <ningersoll96@gmail.com>
2017-04-22 02:06:29 -0400

    fixed formatting

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -6,13 +6,13 @@ time=$(date +%R)
 hostname=$(hostname -s)
 archive="$time_$hostname.tgz"
 
-echo "Backing up $backup_files ..."
+echo -e "Backing up $backup_files ..."
 tar czf $dest/$archive $backup_files
 
-echo
-echo "Backup complete, please transfer the tarball to the backup server! Use this command: \nscp $archive team7@192.168.7.69:/opt/backups/"
+echo -e
+echo -e "Backup complete, please transfer the tarball to the backup server! Use this command: \nscp $archive team7@192.168.7.69:/opt/backups/"
 
-echo "\nCheck with Nick or Joey (the capitan guys for team 7 and 6) to make sure the IP hasn't changed on the backup server. It is also protecting our network with an IDS that will need to be stopped before the transfer."
+echo -e "\nCheck with Nick or Joey (the capitan guys for team 7 and 6) to make sure the IP hasn't changed on the backup server. It is also protecting our network with an IDS that will need to be stopped before the transfer."
 
-echo "Try to run backups at least once every hour incase something breaks"
+echo -e "\nTry to run backups at least once every hour incase something breaks"
 
