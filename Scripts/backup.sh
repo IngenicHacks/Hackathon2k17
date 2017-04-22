db50cf2f435f33a44162ad1c921b7f79e4422159
Nick Ingersoll <ningersoll96@gmail.com>
2017-04-22 01:05:20 -0400

    initial commit

---
+++ Scripts/backup.sh
@@ -0,0 +1,16 @@
+backup_files="/home /etc /root /var /opt"
+dest=$(pwd)
+time=$(date +%R)
+hostname=$(hostname -s)
+archive="$time_$hostname.tgz"
+
+echo "Backing up $backup_files ..."
+tar czf $dest/$archive $backup_files
+
+echo
+echo "Backup complete, please transfer the tarball to the backup server! Use this command: \nscp $archive team7@192.168.7.69:/opt/backups/"
+
+echo "\nCheck with Nick or Joey (the capitan guys for team 7 and 6) to make sure the IP hasn't changed on the backup server. It is also protecting our network with an IDS that will need to be stopped before the transfer."
+
+echo "Try to run backups at least once every hour incase something breaks"
+
